d87987b51fdf1f6cf10bb9fd69aa77d64164766d
Ardian Syla <ardian.syla@studentet.uni-pr.edu>
2021-01-01 23:17:12 +0100

    Fixed home_page slides with images

--- script/home-page.js
+++ script/home-page.js
@@ -1,43 +1,40 @@
-let slideIndex = 0;
-showSlide(slideIndex);
-
-
 function currentSlide(num) {
-    console.log('current ', num);
-    showSlide((slideIndex = num))
-}
-
-function showSlide(n) {
-    console.log(n, " n")
-    let i;
-    let slides = document.getElementsByClassName('mySlide');
+    document.getElementById("rec1").style.backgroundColor = "#fe9900"
     let rectangles = document.getElementsByClassName('rectangle')
-
-    if(n > slides.length){
-        slideIndex = 1;
-    }
-    if(n < 1 ){
-        slideIndex = slides.length
-    }
-
-    for(i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
     for(i = 0; i < rectangles.length; i++) {
         rectangles[i].className = rectangles[i].className.replace(" active", "")
     }
 
-    console.log(slides);
-    console.log(slides['HTMLCollection'])
-    // console.log(slideIndex);
-    // console.log(rectangles);
-    // console.log(slides[slideIndex - 1]);
+    let slides = document.getElementsByClassName("mySlide")
+    for(i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
 
-    // if(slideIndex!=-1){
-    console.log("hini")
-    // slides[slideIndex].style.display = "block";
-    rectangles[slideIndex].className += " active";
-    // }
-    
-    
+    switch(num) {
+    case 1:
+        document.getElementById('slide1').style.display = "block"
+        document.getElementById('rec1').className += " active"
+        document.getElementById("rec1").style.backgroundColor = "#ffffff"
+        break;
+    case 2:
+        document.getElementById('slide2').style.display = "block"
+        document.getElementById('rec2').className += " active"
+        break;
+    case 3:
+        document.getElementById('slide3').style.display = "block"
+        document.getElementById('rec3').className += " active"
+        break;
+    case 4:
+        document.getElementById('slide4').style.display = "block"
+        document.getElementById('rec4').className += " active"
+        break;
+    case 5:
+        document.getElementById('slide5').style.display = "block"
+        document.getElementById('rec5').className += " active"
+        break;
+    default:
+        document.getElementById('slide1').style.display = "block"
+        document.getElementById('rec1').className += " active"
+        document.getElementById("rec1").style.backgroundColor = "#ffffff"
+    }
 }
